8d5c2396cdccfd6f24845195fc4f3864b22615da
zpz <zepu.zhang@gmail.com>
2022-11-05 18:00:44 -0700

    format

--- src/upathlib/util.py
+++ src/upathlib/util.py
@@ -9,23 +9,27 @@ from .gcp import GcpBlobUpath
 PathType = Union[str, Path, Upath]
 
 
-def resolve_path(path: PathType,
-                 *,
-                 thread_pool_executors=None,
-                 bucket_name=None,
-                 project_id=None,
-                 credentials=None,
-                 ):
+def resolve_path(
+    path: PathType,
+    *,
+    thread_pool_executors=None,
+    bucket_name=None,
+    project_id=None,
+    credentials=None,
+):
     if isinstance(path, str):
-        if path.startswith('gs://'):
-            return GcpBlobUpath(path,
-                                thread_pool_executors=thread_pool_executors,
-                                bucket_name=bucket_name,
-                                project_id=project_id,
-                                credentials=credentials,
+        if path.startswith("gs://"):
+            return GcpBlobUpath(
+                path,
+                thread_pool_executors=thread_pool_executors,
+                bucket_name=bucket_name,
+                project_id=project_id,
+                credentials=credentials,
             )
         path = Path(path)
     if isinstance(path, Path):
-        return LocalUpath(str(path.absolute()), thread_pool_executors=thread_pool_executors)
+        return LocalUpath(
+            str(path.absolute()), thread_pool_executors=thread_pool_executors
+        )
     assert isinstance(path, Upath)
     return path
